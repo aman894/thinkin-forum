d504319e68e0fa50ba51996bf1bb55ce50d5d3cf
aman894 <amandeep.malhotra.894@gmail.com>
2016-06-14 21:26:35 +0530

    signup added

--- app/src/main/java/com/aman/thinkin/MainActivity.java
+++ app/src/main/java/com/aman/thinkin/MainActivity.java
@@ -1,135 +1,43 @@
 package com.aman.thinkin;
 
 
-import android.app.ProgressDialog;
-import android.content.Intent;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
-
-import com.firebase.client.AuthData;
 import com.firebase.client.Firebase;
-import com.firebase.client.FirebaseError;
-
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-    Firebase mRootRef;
-    String loginEmail,loginPassword;
-    ProgressDialog progressDialog;
-    EditText etLoginEmail,etLoginPassword;
-    Button bLogin;
+    Button bToSignup;
+    FragmentManager fragmentManager;
+    FragmentTransaction fragmentTransaction;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Firebase.setAndroidContext(this);
-        setUpVariables();
-        mRootRef = new Firebase("https://think-in.firebaseio.com/");
-        bLogin.setOnClickListener(this);
-        /*----------remove this--------------*/
-        mRootRef.authWithPassword("aman@gmail.com", "aman", new Firebase.AuthResultHandler() {
-            @Override
-            public void onAuthenticated(AuthData authData) {
-                Log.v("E_LOGIN", "Logged in");
-                //progressDialog.dismiss();
-                Log.w("F_PROVIDER",authData.getProvider());
-                Toast.makeText(MainActivity.this, "Logged in !!", Toast.LENGTH_SHORT).show();
-                Intent openHomeActivity = new Intent(MainActivity.this,HomeActivity.class);
-                startActivity(openHomeActivity);
-            }
-
-            @Override
-            public void onAuthenticationError(FirebaseError firebaseError) {
-                //progressDialog.dismiss();
-                Log.v("E_LOGIN", "Logged in failed with code " + firebaseError.getCode());
-                Toast.makeText(MainActivity.this, "Login failed", Toast.LENGTH_SHORT).show();
-            }
-        });
-        /*----------------------------------------------*/
-
-        /*
-        final MaterialLoginView login = (MaterialLoginView) findViewById(R.id.login);
-        login.setListener(new MaterialLoginViewListener() {
-            @Override
-            public void onRegister(TextInputLayout registerUser, TextInputLayout registerPass, TextInputLayout registerPassRep) {
-                //Handle register
-                final CharSequence userID=registerUser.getEditText().getText().toString();
-                mRootRef.createUser(registerUser.getEditText().getText().toString(), registerPass.getEditText().getText().toString(),
-                        new Firebase.ResultHandler() {
-                    @Override
-                    public void onSuccess() {
-                        Toast.makeText(MainActivity.this, "Registered", Toast.LENGTH_SHORT).show();
-                        Firebase userRef = mRootRef.child("users");
-                    }
-
-                    @Override
-                    public void onError(FirebaseError firebaseError) {
-                        Toast.makeText(MainActivity.this, "Registration failed", Toast.LENGTH_SHORT).show();
-                    }
-                });
-            }
-
-
-            }
-        });
-        */
-
-        Log.v("Value of root", mRootRef.toString());
-
-    }
-
-    private void setUpVariables() {
-        etLoginEmail = (EditText)findViewById(R.id.etLoginEmail);
-        etLoginPassword = (EditText)findViewById(R.id.etLoginPassword);
-        bLogin = (Button) findViewById(R.id.bLogin);
+        bToSignup = (Button)findViewById(R.id.bToSignup);
+        bToSignup.setOnClickListener(this);
+        fragmentManager = getSupportFragmentManager();
+        fragmentTransaction = fragmentManager.beginTransaction();
+        LoginFragment loginFragment = new LoginFragment();
+        fragmentTransaction.add(R.id.flMainContainer,loginFragment);
+        fragmentTransaction.commit();
     }
 
     @Override
     public void onClick(View view) {
         switch (view.getId()){
-            case R.id.bLogin:
-                Pattern emailPattern = Pattern.compile("[0-9]{7}@(kiit.ac.in)");
-                Matcher emailMatcher = emailPattern.matcher(etLoginEmail.getText().toString());
-               // if(emailMatcher.matches()){
-                    Log.v("E_LOGIN ATTEMPT", "Attempting login with email: " + etLoginEmail.getText().toString());
-                    Log.v("E_LOGIN ATTEMPT", "Attempting login with password: " + etLoginPassword.getText().toString());
-                    loginEmail=etLoginEmail.getText().toString();
-                    loginPassword=etLoginPassword.getText().toString();
-                    progressDialog = new ProgressDialog(MainActivity.this);
-                    progressDialog.setTitle("Attempting Log in");
-                    progressDialog.setMessage("Please wait..");
-                    progressDialog.setIndeterminate(true);
-                    progressDialog.setCancelable(false);
-                    progressDialog.show();
-                    mRootRef.authWithPassword(loginEmail, loginPassword, new Firebase.AuthResultHandler() {
-                        @Override
-                        public void onAuthenticated(AuthData authData) {
-                            Log.v("E_LOGIN", "Logged in");
-                            progressDialog.dismiss();
-                            Log.w("F_PROVIDER",authData.getProvider());
-                            Toast.makeText(MainActivity.this, "Logged in !!", Toast.LENGTH_SHORT).show();
-                            Intent openHomeActivity = new Intent(MainActivity.this,HomeActivity.class);
-                            startActivity(openHomeActivity);
-                        }
-
-                        @Override
-                        public void onAuthenticationError(FirebaseError firebaseError) {
-                            progressDialog.dismiss();
-                            Log.v("E_LOGIN", "Logged in failed with code " + firebaseError.getCode());
-                            Toast.makeText(MainActivity.this, "Login failed", Toast.LENGTH_SHORT).show();
-                        }
-                    });
-              //  }
-               // else
-                   // Toast.makeText(this,"Please enter a valid KIIT email ID",Toast.LENGTH_SHORT).show();
-
+            case R.id.bToSignup:
+                SignupFragment signupFragment = new SignupFragment();
+                fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.setCustomAnimations(R.anim.slide_from_right,R.anim.slide_to_left);
+                fragmentTransaction.replace(R.id.flMainContainer,signupFragment);
+                fragmentTransaction.commit();
+                bToSignup.setVisibility(View.INVISIBLE);
                 break;
 
         }
---
+++ app/src/main/java/com/aman/thinkin/SignupFragment.java
@@ -0,0 +1,67 @@
+package com.aman.thinkin;
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+
+public class SignupFragment extends Fragment implements View.OnClickListener{
+    Firebase mRootRef;
+    Button bSignup;
+    EditText etSignupEmail,etSignupPassword;
+    ProgressDialog progressDialog;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_signup, container, false);
+        bSignup = (Button)view.findViewById(R.id.bSignup);
+        etSignupEmail = (EditText)view.findViewById(R.id.etSignupEmail);
+        etSignupPassword = (EditText)view.findViewById(R.id.etSignupPassword);
+        bSignup.setOnClickListener(this);
+        return view;
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.bSignup:
+                String email = etSignupEmail.getText().toString();
+                String password = etSignupPassword.getText().toString();
+                progressDialog = new ProgressDialog(getActivity());
+                progressDialog.setTitle("Registering");
+                progressDialog.setMessage("Please wait..");
+                progressDialog.setIndeterminate(true);
+                progressDialog.setCancelable(false);
+                progressDialog.show();
+                mRootRef = new Firebase("https://think-in.firebaseio.com/");
+                mRootRef.createUser(email, password, new Firebase.ResultHandler() {
+                    @Override
+                    public void onSuccess() {
+                        Toast.makeText(getActivity(), "Registered", Toast.LENGTH_SHORT).show();
+                        progressDialog.dismiss();
+                    }
+
+                    @Override
+                    public void onError(FirebaseError firebaseError) {
+                        Toast.makeText(getActivity(), "Registration failed", Toast.LENGTH_SHORT).show();
+                        progressDialog.dismiss();
+                    }
+                });
+                break;
+        }
+    }
+}
